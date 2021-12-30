21cb98c1eae2c94993ebde7d64c5ccf19eb790ff
BHessel <hesselfitness@gmail.com>
2021-12-29 19:55:15 -0500

    attempt 1 w/ aws integration, failed

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :videos
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'auth/sign_in', to: 'auth#sign_in'
+  post 'auth/sign_out', to: 'auth#sign_out'
 end
